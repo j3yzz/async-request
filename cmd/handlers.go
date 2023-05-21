c68cb2d21e1ca389abb11adb61c86874d24280c4
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-05-21 12:24:48 +0330

    add logic to handler

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,7 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Payload struct {
+	Data any
+}
 
 func (app *Config) async(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	response := make(map[string]any)
+	response["success"] = true
+
+	rc, _ := app.Redis.Get(app.context, "request_counter").Result()
+	response["old_rc"] = rc
+
+	app.Redis.Incr(app.context, "request_counter")
+	rc, _ = app.Redis.Get(app.context, "request_counter").Result()
+	response["new_rc"] = rc
+
+	p := Payload{Data: response}
+
+	json.NewEncoder(w).Encode(p)
 }
